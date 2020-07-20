33e7116994f71ab6c40bb3c5fe48044ce0d32005
sanpedrofabianezequiel <62151955+sanpedrofabianezequiel@users.noreply.github.com>
2020-07-20 19:25:43 -0300

    Update algorithm.js

--- JsEvents/algorithm.js
+++ JsEvents/algorithm.js
@@ -28,11 +28,18 @@ function comparacion(){                       //Comparacion simple
     }
 }             
 function comparacionEasy() {                    //Comparacion Caracter a caracter
+  var valor1=0;
+  var valor2=0;
       for(i =0;i< email.value.length  ;i++){
+     
         if (email.value.charAt(i) == emailVerificacion.value.charAt(i) ) {
-            verificacion= true
-        }else{ verifcacion= false }      
+             valor1 += 1
+        }else{ valor2 +=1 }      
       } 
+      if(valor1>valor2){
+          verificacion=true;
+      }else{verificacion=false}
+
 }
 function execute(verifcacion){                    //ejecuta las clases
     if(verificacion){
